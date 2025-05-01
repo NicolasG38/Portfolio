5c84d39ef456b75a89f80fca13b27018953f60a4
Nicolas GIRARD <NG38@outlook.fr>
2025-05-01 17:37:15 +0200

    feature/ajout de la section Parcours. élément stylisé

---
+++ Nicolasgirard.me/portfolio/src/Components/PersonalProject.tsx
@@ -0,0 +1,34 @@
+import './PersonalProject.css'
+
+
+
+function PersonalProject() {
+    return (
+        <>
+            <h1>PROJETS PERSONNEL</h1>
+
+            <section className={"projectSection"}>
+                <img className={"projectImg"} src={"src/assets/images/Resource/Project_1.png"} alt="personal-project 1" />
+                <img className={"projectImg"} src={"src/assets/images/Resource/Project_2.png"} alt="personal-project 2" />
+            </section>
+            <section className={"projectSection"}>
+                <img className={"projectImg"} src={"src/assets/images/Resource/Project_3.png"} alt="personal-project 3" />
+                <section className={"boxProject"}>
+                    <p>.04</p>
+                    <h3>EN COURS DE DÉVELOPPEMENT</h3>
+                    <ul className={"pojectUl"}>
+                        <li>SPIRE-PROJECT <span>(Soluce de JV - REACT)</span></li>
+                    </ul>
+                    <h3>À VENIR...</h3>
+                    <ul>
+                        <li>TheLazyAgency <span>(Site web de mon agence - REACT)</span></li>
+                        <li>NASCAR-PROJECT<span>(Réseaux social - REACT)</span></li>
+                    </ul>
+                </section>
+            </section>
+
+        </>
+    )
+}
+
+export default PersonalProject
