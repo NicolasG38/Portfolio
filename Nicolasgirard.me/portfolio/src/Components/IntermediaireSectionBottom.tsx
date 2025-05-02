0743a62b15213d5aceefac93e61e43b2a4232663
Nicolas GIRARD <NG38@outlook.fr>
2025-05-02 11:34:05 +0200

    feature/ajout de la section intermédiaire en bas du site

---
+++ Nicolasgirard.me/portfolio/src/Components/IntermediaireSectionBottom.tsx
@@ -0,0 +1,15 @@
+import './IntermediaireSectionBottom.css'
+
+function IntermediaireSectionBottom(){
+    return(
+        <>
+            <section className={"ISBSection"}>
+                <h2 className={"ISBid ISBFirstName"}>Nicolas</h2>
+                <img className={"ISBimg"} src={"src/assets/images/NicolasG38-NG.png"} alt="Nicolas" />
+                <h2 className={"ISBid ISBName"}>GIRARD</h2>
+            </section>
+        </>
+    )
+}
+
+export default IntermediaireSectionBottom;
